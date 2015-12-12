64eccfc5f336f7496a51ae875571363bbb4045e5
Jared Wright <jawerty@Jareds-MacBook-Air.local>
2015-12-12 04:15:20 -0500

    Socket.io

--- static/js/reactive/src/social-component.js
+++ static/js/reactive/src/social-component.js
@@ -1,4 +1,19 @@
 window.SocialComponent = React.createClass({
+  componentDidMount: function() {
+    var canonical_url = "http://localhost:5000/"; //should be commented out
+    window.socket = io(canonical_url);
+
+    window.socket.emit("joinChannel", {channel_name: "channel name"});
+    //window.socket.emit("leaveChannel", {channel_name: "channel name"})
+    
+    window.socket.on('userJoined', function(new_user_count){
+
+    });
+    window.socket.on('userLeft', function(new_user_count){
+
+    });
+    
+  },
   propTypes: {
     pushed: React.PropTypes.bool.isRequired,
   },
