5acaa8d09f2229ecf0f2b25d1c4aed42e8579683
Jared Wright <jawerty@Jareds-MacBook-Air.local>
2015-12-14 20:25:45 -0500

    ok

--- utils/db.py
+++ utils/db.py
@@ -9,7 +9,7 @@ port = config.db["port"]
 mongo_url = 'mongodb://'+host+':'+port+'/'
 database_name = config.db["dbname"]
 
-client = MongoClient(mongo_url, connect=False)
+client = MongoClient(mongo_url)
 db = client[database_name]
 
 def getHashtag(channel_name):
