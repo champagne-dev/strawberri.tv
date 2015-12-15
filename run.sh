0721af11a928b454252f9e71c6ffec7e39f608ae
Jared Wright <jawerty210@gmail.com>
2015-12-15 13:25:58 -0500

    run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-gunicorn --worker-class eventlet --bind 127.0.0.1:8000 __init__:app --log-file=/var/log/gunicorn-strawberri.log
+gunicorn --worker-class eventlet --bind 127.0.0.1:8000 __init__:app --log-file=/var/log/gunicorn-strawberri.log &
+tail -f /var/log/gunicorn-strawberri.log
