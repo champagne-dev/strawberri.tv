cbbcc4e657b8e079931e9f3bbeebeaf4a69b6f56
Jared Wright <jawerty@Jareds-MacBook-Air.local>
2015-12-14 22:28:23 -0500

    ok

--- __init__.py
+++ __init__.py
@@ -115,18 +115,18 @@ try:
 except:
     db.init_channels()
 
-if __name__ == "__main__":
-    # args = parser.parse_args()
-    # if args.cron:
-    #     def close_handler():
-    #         c.exit()
+# if __name__ == "__main__":
+#     # args = parser.parse_args()
+#     # if args.cron:
+#     #     def close_handler():
+#     #         c.exit()
 
-    #     atexit.register(close_handler)
-    #     cwd = os.path.dirname(os.path.realpath(__file__))
-    #     c.run(cwd+"/pushURL.py", False)
-    socketio.run(app, debug=config.server["debug"], host=config.server["host"], port=config.server["port"])    
-    from clock import run as clock_run
-    clock_run()
+#     #     atexit.register(close_handler)
+#     #     cwd = os.path.dirname(os.path.realpath(__file__))
+#     #     c.run(cwd+"/pushURL.py", False)
+socketio.run(app, debug=config.server["debug"], host=config.server["host"], port=config.server["port"])    
+from clock import run as clock_run
+clock_run()
 # else:
 #     try:
 #         socketio.run(app, debug=config.server["debug"], host=config.server["host"], port=config.server["port"])
